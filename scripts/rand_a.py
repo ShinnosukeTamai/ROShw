d19a43ee75353fcb9692d4ef44c056504cec8de4
shinnosuke tamai <76773901+ShinnosukeTamai@users.noreply.github.com>
2021-01-21 13:25:17 +0900

    Update rand_a.py

--- scripts/rand_a.py
+++ scripts/rand_a.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 #license:BSD-3-Clause-License
 #Copyright (c) 2020, Shinnosuke Tamai & Ryuichi Ueda.
-#All rights reserved.                                                                                                                                                          
+#All rights reserved.                                                                                                                                        
 import rospy
 import random
 import string
@@ -9,14 +9,16 @@ from std_msgs.msg import Int32
 
 rospy.init_node("prac")
 pub = rospy.Publisher('prac_up', Int32, queue_size=1)
+#rate = rospy.rate(10)
+
 dat = string.ascii_lowercase#+string.ascii_uppercase
 
 T1 = 0
-T2 = 0
+F = 0
 NOT = 0
-while NOT < 10:
+while NOT < 5:
     NOT += 1
-    dat1=''.join([random.choice(dat) for i in range(6)])
+    dat1=''.join([random.choice(dat) for i in range(4)])
     print(dat1)
     ans = input("GO!:")
 
@@ -26,7 +28,9 @@ while NOT < 10:
         T1 += 1
     else :
         print("False")
-        T2 += 1
+        F += 1
+        pub.publish(F)
+        #rate.sleep()
 
 
 print("finsh! true:", T1," number of times:", NOT)
